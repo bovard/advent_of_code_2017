94092b22c02b6dc2603ff951fedc81ac2e0f00a6
Bovard Doerschuk-Tiberi <bovard.tiberi@gmail.com>
2017-12-20 17:15:10 -0800

    eval ftw

--- 08/1/solution.py
+++ 08/1/solution.py
@@ -1,31 +1,5 @@
 
-LT = "<"
-GT = ">"
-GTE = ">="
-LTE = "<="
-NE = "!="
-E = "=="
-
-INC = "inc"
-DEC = "dec"
-
 from collections import defaultdict
-
-def comp(var_val, comp, val):
-	if comp == LT:
-		return var_val < val
-	elif comp == GT:
-		return var_val > val
-	elif comp == LTE:
-		return var_val <= val
-	elif comp == GTE:
-		return var_val >= val
-	elif comp == NE:
-		return var_val != val
-	elif comp == E:
-		return var_val == val
-	assert False, "comp not found {}".format(op)
-
 
 def run(lines):
 	register = defaultdict(int)
@@ -34,13 +8,13 @@ def run(lines):
 		parts = line.split(' ')
 		assert len(parts) == 7
 		assert parts[3] == 'if'
-		assert parts[5] in (LT, GT, GTE, LTE, NE, E)
 
-		var_val = register.get(parts[4])
-		if comp(var_val, parts[5], int(parts[6])):
-			if parts[1] == INC:
+		con_val = register.get(parts[4])
+		con_str = "{} {} {}".format(con_val, parts[5], int(parts[6]))
+		if eval(con_str):
+			if parts[1] == 'inc':
 				register[parts[0]] += int(parts[2])
-			elif parts[1] == DEC:
+			elif parts[1] == 'dec':
 				register[parts[0]] -= int(parts[2])
 			else:
 				assert False, "op not found"
